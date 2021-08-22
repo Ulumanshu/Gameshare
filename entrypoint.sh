e34e709b3a5868074cdd347a3771890cfd03ed85
Aukse <aukse@Aukses-MacBook-Air.local>
2021-08-22 13:45:28 +0200

    add personal aukse

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python /code/gameshare/manage.py makemigrations
-python /code/gameshare/manage.py migrate
-python /code/gameshare/manage.py runserver 0.0.0.0:8000
+python /code/aukse/manage.py makemigrations
+python /code/aukse/manage.py migrate
+python /code/aukse/manage.py runserver 0.0.0.0:8000
